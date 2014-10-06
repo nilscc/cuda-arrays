876ff6765aed615ecdbe6e159051d57272770954
Nils Schweinsberg <mail@nils.cc>
2014-10-06 15:12:03 +0200

    Fix dep gen

---
+++ test/2d.cu
@@ -0,0 +1,39 @@
+#include <CuArrays>
+#include <cassert>
+
+namespace Test
+{
+    __constant__ CuArrays::Array2D<int> test;
+}
+
+__global__ void device_test()
+{
+    using namespace Test;
+
+    assert(test(0,0) == 1);
+    assert(test(1,1) == 1);
+}
+
+void host_test()
+{
+    CuArrays::ManagedArray2D<int> test(Test::test);
+
+    test.malloc(2, 2);
+
+    for (int i = 0; i < 2; i++)
+        test(i,i) = 1;
+
+    assert(test(0,0) == 1);
+    assert(test(1,1) == 1);
+
+    // test.copyToDevice();
+}
+
+int main()
+{
+
+    host_test();
+    // device_test<<<1,1>>>();
+
+    return 0;
+}
